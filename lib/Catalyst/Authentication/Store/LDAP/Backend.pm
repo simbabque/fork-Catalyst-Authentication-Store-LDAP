dfead577d0091c13ab636431aefca08d0ff7330b
Dagfinn Ilmari Mannsåker <ilmari@ilmari.org>
2015-02-20 18:05:40 +0000

    Version 1.015

--- lib/Catalyst/Authentication/Store/LDAP/Backend.pm
+++ lib/Catalyst/Authentication/Store/LDAP/Backend.pm
@@ -72,7 +72,7 @@ use base qw( Class::Accessor::Fast );
 use strict;
 use warnings;
 
-our $VERSION = '1.014';
+our $VERSION = '1.015';
 
 use Catalyst::Authentication::Store::LDAP::User;
 use Net::LDAP;
